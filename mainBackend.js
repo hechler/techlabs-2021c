df06b630cde076af37823663be15719f4f157abf
Henning Ilbertz <henningilbe21@gmail.com>
2021-08-29 15:46:27 +0200

    Backend

--- mainBackend.js
+++ mainBackend.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const server = express()
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
@@ -18,28 +18,25 @@ mongoose.connect('mongodb://localhost:27017/Länder', { useNewUrlParser: true, u
 //Mongodb auf default server integriert mit Datenbank "Länder"
 
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) => {
-    res.send("HI")
+server.get('/', (req, res) => {
+    res.send("Startseite")
 })
 
-app.get('/countries', async (req, res) => {
+server.get('/countries', async (req, res) => {
     const countries = await Country.find({})
     res.render('Index', { countries })
 })
 
-app.get('/:name', async (req, res) => {
+server.get('/:name', async (req, res) => {
     const country = await Country.findOne({ name: req.params.name })
     res.render('Land', { country })
-    console.log(country)
-    console.log(req.params)
-
 })
 
 
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log("Listening on Port 3000")
 })
